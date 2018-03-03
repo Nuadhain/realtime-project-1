4a9e7c2b1b53bb08490a0f67bbdbe44a42072208
Jason Noone <jsnnn497@gmail.com>
2018-03-03 14:30:53 -0500

    Add files via upload
    Finished project 1

--- src/server.js
+++ src/server.js
@@ -26,11 +26,12 @@ const rect = {
   color: 'black',
 };
 
-const user = {
-  name: 'default',
-};
+const objectives = ['Make sure the entire wall is painted', 'Make sure the entire wall is one color', 'Pick a color and make sure it stays on the wall', 'Just before the paint dries change the color of the wall', 'Draw an image on the wall at least once', 'Come up with your own objective but do not let your friends know what it is', 'Start a Bolshevik revolution'];
+
+const users = [];
 
 let time = 0;
+let key = 0;
 
 io.on('connect', (socket) => {
   socket.join('room');
@@ -50,13 +51,33 @@ io.on('connect', (socket) => {
   });
 
   socket.on('join', (data) => {
-    user.name = data;
+    users[key] = data;
+
+    if (key === 0) {
+      socket.emit('setHost', true);
+    }
+
+    key++;
     const message = {
-      msg: `${user.name} has joined`,
+      msg: `${data.name} has joined`,
+      name: 'server',
+    };
+
+    io.sockets.in('room').emit('setKey', key);
+    io.sockets.in('room').emit('receiveMsg', message);
+  });
+
+  socket.on('disconnect', (data) => {
+    const message = {
+      msg: `${data.name} has disconnected`,
       name: 'server',
     };
 
     io.sockets.in('room').emit('receiveMsg', message);
+
+    socket.leave('room');
+
+    delete users[data.key];
   });
 
   socket.on('sendMsg', (data) => {
@@ -66,5 +87,29 @@ io.on('connect', (socket) => {
     };
 
     io.sockets.in('room').emit('receiveMsg', message);
+  });
+
+  socket.on('readied', (data) => {
+    let usersReady = 0;
+
+    if (users[data.key] === data) {
+      users[data.key].name = data.name;
+      users[data.key].ready = data.ready;
+    }
+    if (users[data.key].ready) {
+      usersReady++;
+      // Send Users an objective
+      const objv = objectives[Math.floor((Math.random() * objectives.length))];
+      const sendData = {
+        user: users[data.key],
+        objective: objv,
+      };
+
+      io.sockets.in('room').emit('receiveObjective', sendData);
+    }
+
+    if (usersReady === users.length && users.length >= 4) {
+      io.sockets.in('room').emit('allReady', usersReady);
+    }
   });
 });
